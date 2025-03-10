c258c6d2483f4e3ea6772d57a25050da23bc6e99
Aaronleon13 <aaronleon13@gmail.com>
2025-03-10 10:54:43 -0700

    Agregar persistencia del tema oscuro en localStorage

--- js/script.js
+++ js/script.js
@@ -9,9 +9,15 @@ swapIconTheme();
 const swapIconTheme = () => {
     if (body.classList.contains('dark-theme')) {
         button.innerText = '🌞';
+        localStorage.setItem('theme', 'dark');
     }else {
         button.innerText = '🌕';
+        localStorage.removeItem('theme');
     }
+}
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme');    
 }
 
 swapIconTheme();
