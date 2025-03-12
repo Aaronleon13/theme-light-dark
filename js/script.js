7cfb7800ac24da9c3b6c7a05969be98ef854edce
Aaronleon13 <aaronleon13@gmail.com>
2025-03-12 10:54:54 -0700

    Refactor manejo de tema oscuro en script.js para mejorar legibilidad y mantenimiento

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 const body = document.querySelector('body');
 const button = document.querySelector('button');
 
+const THEME_DARK = 'dark-theme';
+const KEY_LOCALSTORAGE = 'theme';
+const VALUE_LOCALSTORAGE = 'dark';
+const ICON_DARK = '🌞';
+const ICON_LIGHT = '🌕';
+
 button.addEventListener('click',() => {
-body.classList.toggle('dark-theme');
+body.classList.toggle(THEME_DARK);
 swapIconTheme();
 })
 
 const swapIconTheme = () => {
-    if (body.classList.contains('dark-theme')) {
-        button.innerText = '🌞';
-        localStorage.setItem('theme', 'dark');
+    if (body.classList.contains(THEME_DARK)) {
+        button.innerText = ICON_DARK;
+        localStorage.setItem(KEY_LOCALSTORAGE, VALUE_LOCALSTORAGE);
     }else {
-        button.innerText = '🌕';
-        localStorage.removeItem('theme');
+        button.innerText = ICON_LIGHT;
+        localStorage.removeItem(KEY_LOCALSTORAGE);
     }
 }
 
-if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-theme');    
+const initialTheme = () => {
+    if (localStorage.getItem(KEY_LOCALSTORAGE) === VALUE_LOCALSTORAGE) {
+        body.classList.add(THEME_DARK);    
+    }
+    swapIconTheme();
 }
 
-swapIconTheme();
+initialTheme();
+
