0504789b5fe4aa357f831a3cf93549fec321e992
AaronLeon13 <aaronleon13@gmail.com>
2025-03-13 19:44:11 -0700

    Actualizar iconos de tema oscuro y claro en script.js para mejorar la claridad visual

--- js/script.js
+++ js/script.js
@@ -4,28 +4,32 @@ const button = document.querySelector('button');
 const THEME_DARK = 'dark-theme';
 const KEY_LOCALSTORAGE = 'theme';
 const VALUE_LOCALSTORAGE = 'dark';
-const ICON_DARK = '🌞';
-const ICON_LIGHT = '🌕';
+const ICON_DARK = '🌙';
+const ICON_LIGHT = '☀️';
 
-button.addEventListener('click',() => {
-body.classList.toggle(THEME_DARK);
-swapIconTheme();
+button.addEventListener('click', () => {
+    body.classList.toggle(THEME_DARK);
+    swapIconTheme();
 })
 
 const swapIconTheme = () => {
     if (body.classList.contains(THEME_DARK)) {
-        button.innerText = ICON_DARK;
+        button.innerText = ICON_LIGHT;
         localStorage.setItem(KEY_LOCALSTORAGE, VALUE_LOCALSTORAGE);
     }else {
-        button.innerText = ICON_LIGHT;
+        button.innerText = ICON_DARK;
         localStorage.removeItem(KEY_LOCALSTORAGE);
     }
 }
 
-const initialTheme = () => {
+const verifyLocalStorage = () => {
     if (localStorage.getItem(KEY_LOCALSTORAGE) === VALUE_LOCALSTORAGE) {
-        body.classList.add(THEME_DARK);    
+        body.classList.add(THEME_DARK);
     }
+}   
+
+const initialTheme = () => {
+    verifyLocalStorage();
     swapIconTheme();
 }
 
